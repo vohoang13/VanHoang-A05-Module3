ea6b8f844cf560cbfa7598ad67e8f446064c98f0
vohoang13 <vanhoang132003@gmail.com>
2022-11-30 16:42:20 +0700

    update

--- ss12/bai_tap/SQL/user.sql
+++ ss12/bai_tap/SQL/user.sql
@@ -13,3 +13,24 @@ begin
     where users.id = id;
 end //
 DELIMITER ;
+
+DELIMITER //
+create procedure updatebyname (In username varchar(120), In email varchar(220), In country varchar(120))
+begin 
+	update users set 
+    users.email = email,
+    users.country = country
+    where users.user_name like  username;
+end //
+DELIMITER ;
+
+DELIMITER //
+create procedure deleteuser (In username varchar(120))
+begin 
+	delete from users
+    where users.user_name like username;
+end //
+DELIMITER ;
+
+
+drop procedure updatebyName;
